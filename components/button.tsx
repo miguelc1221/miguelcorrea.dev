f3cae59dbd993f2716221346565d94bf22f018aa
Miguel Correa <miguelc1221@gmail.com>
2022-05-27 17:47:13 -0600

    pass button props

--- components/button.tsx
+++ components/button.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 export const Button = React.forwardRef<
   HTMLButtonElement,
   { children: React.ReactNode }
->(({ children }, ref): JSX.Element => {
+>(({ children, ...props }, ref): JSX.Element => {
   return (
     <button
       className="px-4 py-2 text-sm bg-amaranth text-white shadow-sm hover:bg-amaranth-2"
       ref={ref}
+      {...props}
     >
       {children}
     </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -123,13 +123,3 @@ export default function Home(): JSX.Element {
     </>
   )
 }
-
-{
-  /* <Link href="#projects" passHref>
-<button className="relative px-6 py-3 font-bold text-black group max-w-fit">
-  <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
-  <span className="absolute inset-0 w-full h-full border-4 border-black"></span>
-  <span className="relative">See my work</span>
-</button>
-</Link> */
-}
